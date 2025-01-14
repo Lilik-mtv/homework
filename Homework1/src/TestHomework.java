47f9ef86c1d6f855cc120d4c151625d77c7aaa2d
Lilik-mtv <lilic999@yyahoo.com>
2025-01-13 18:06:39 -0800

    This is a commit test nr3

--- Homework1/src/TestHomework.java
+++ Homework1/src/TestHomework.java
@@ -1,5 +1,6 @@
 public class TestHomework {
     public static void main(String[] args) {
         System.out.println("Test"); // Comentariu testat
+        // Testarea trei
     }
 }
--- Homework1/src/TestHomework2.java
+++ Homework1/src/TestHomework2.java
@@ -1,5 +1,6 @@
 public class TestHomework2 {
     public static void main(String[] args) {
         // This is a second test
+        // Testarea trei
     }
 }
